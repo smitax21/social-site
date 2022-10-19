08dba1e333a0b622af67a415ad8d50a1af13b842
Smita Pandya <60938493+smitax21@users.noreply.github.com>
2022-10-19 14:54:39 +0100

    Added the Router in the index.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import AddPost from "./Components/AddPost/AddPost";
 import logo from "./logo.svg";
 import "./App.css";
+
+// function updateMyValues(value) {}
 
 function App() {
   return (
     <>
       <div>
+        {/* <Routes
+          path="/AddPost"
+          index
+          element={
+            <AddPost //functionFromParent={(value) => updateMyValues(value)}
+            />
+          }
+        ></Routes> */}
         <AddPost />
       </div>
     </>
