e2d90f8eb2cae6c75e5eac37101869a9d904e769
Smita Pandya <60938493+smitax21@users.noreply.github.com>
2022-10-19 23:35:29 +0100

    added post data into localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import AddPost from "./Components/AddPost/AddPost";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // saving post in the localhost
+  useEffect(() => {
+    // localStorage.setItem("data", JSON.stringify(data));
+    const items = JSON.parse(localStorage.getItem("data"));
+    // setData(JSON.parse(items) || []);
+    console.log(items);
+  }, []);
+
+  function updateMyValues(value) {
+    setData((prev) => {
+      let newState = [...prev, value];
+      localStorage.setItem("data", JSON.stringify(newState));
+      return newState;
+    });
+  }
+
   return (
     <>
       <div>
-        <AddPost />
+        <Routes>
+          <Route
+            path="/"
+            index
+            element={
+              <AddPost functionFromParent={(val) => updateMyValues(val)} />
+            }
+          />
+        </Routes>
       </div>
     </>
   );
