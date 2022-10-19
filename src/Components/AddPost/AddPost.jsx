8680ec3be2399c0345a9b9bdf0f12c20bb55cbca
Smita Pandya <60938493+smitax21@users.noreply.github.com>
2022-10-19 11:22:54 +0100

    Added the the addpost form template

---
+++ src/Components/AddPost/AddPost.jsx
@@ -0,0 +1,29 @@
+import Button from "react-bootstrap/Button";
+import React from "react";
+import Form from "react-bootstrap/Form";
+
+const AddPost = () => {
+  return (
+    <>
+      <div className="AddContainer">
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Username..." />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Add post</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="Add here ..." />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Browse</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+          <Button type="submit">Add</Button>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default AddPost;
